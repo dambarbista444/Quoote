7018f883365e5e3b28a12113501cb5ca9c07698c
dambarbista1990 <dambarbista.db@gmail.com>
2020-07-19 17:55:04 -0400

    xcodeProject

---
+++ Evlla/Model/PopularDataModel.swift
@@ -0,0 +1,16 @@
+//
+// PopularDataMode.swift
+//  Evlla
+//
+//  Created by Dambar Bista on 7/17/20.
+//  Copyright © 2020 Dambar Bista. All rights reserved.
+//
+
+import Foundation
+
+
+struct PopularDataModel {
+    
+    var quotes: String
+
+}
